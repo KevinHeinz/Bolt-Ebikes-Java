926188c97055f9b0c1d993d30a04cc446e214860
KevinHeinz <36209706+KevinHeinz@users.noreply.github.com>
2023-05-26 15:46:53 -0500

    Delete 1-database- MySQL initialization scripts directory

--- 1-database- MySQL initialization scripts/1-initialize-user-bolt.sql
+++
@@ -1,4 +0,0 @@
-/* this .sql script initializes my website db user*/
-
-CREATE USER 'bolt-ebikes'@'localhost' IDENTIFIED BY 'bolt-ebikes';
-GRANT ALL PRIVILEGES ON * . * TO 'bolt-ebikes'@'localhost';
--- 1-database- MySQL initialization scripts/5-make-Customer-email-unique.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE customer ADD UNIQUE (email);
